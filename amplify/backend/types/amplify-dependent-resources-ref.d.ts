63ade4a8ab83f8bf7c7f7cb3048d7c044c7c9509
pahirdinhamut <69786935+pahirdinhamut@users.noreply.github.com>
2022-05-22 22:40:37 +0200

    finish

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "EatsDelivryApp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "EatsDelivryApp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/models/index.js
@@ -0,0 +1,24 @@
+// @ts-check
+import { initSchema } from '@aws-amplify/datastore';
+import { schema } from './schema';
+
+const OrderStatus = {
+  "NEW": "NEW",
+  "COOKING": "COOKING",
+  "READY_FOR_PICKUP": "READY_FOR_PICKUP",
+  "PICKED_UP": "PICKED_UP",
+  "COMPELETED": "COMPELETED"
+};
+
+const { Basket, BasketDish, Dish, OrderDish, Order, Restaurant, User } = initSchema(schema);
+
+export {
+  Basket,
+  BasketDish,
+  Dish,
+  OrderDish,
+  Order,
+  Restaurant,
+  User,
+  OrderStatus
+};
